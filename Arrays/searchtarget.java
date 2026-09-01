ad12ca2ae31ec6c0d1452762d9d3d94765fba974
Pallavi Joshi <pallavi15may04@gmail.com>
2026-09-01 14:19:18 +0530

    Add array problems

---
+++ Arrays/searchtarget.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class searchtarget {
+    public static int search(int[]arr,int key){
+        int start=0;
+        int end=arr.length-1;
+        
+    while(start<=end)
+        {
+        int mid=(start+end)/2;
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        else if(arr[mid]<key)
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+public static void main(String args[]){
+    int[] arr = {4, 5, 6, 7, 0, 1, 2};
+    int target = 5;
+    System.out.println(search(arr,target));
+
+    }
+}
+
+
+
+    
+
